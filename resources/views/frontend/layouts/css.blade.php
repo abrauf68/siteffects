61e91571a461aa4c96d667166e1b9f622e70d3f0
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 14:43:24 +0500

    some chnages

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -1,12 +1,4 @@
+<link rel="stylesheet" href="{{ asset('frontAssets/css/main-style.min.css') }}">
 <!-- Toastr CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 <!-- Leaflet CSS -->
-
-<link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/tekmino-icon.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/nice-select.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/swiper.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/venobox.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/leaflet.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/meanmenu.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/main.min.css') }}" />

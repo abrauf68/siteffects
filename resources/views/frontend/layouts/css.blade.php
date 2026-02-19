20e499f725d50e655dddab7cc0fbb44c1b5895fa
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 14:49:19 +0500

    Update css.blade.php

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -1,4 +1,12 @@
-<link rel="preload" rel="stylesheet" href="{{ asset('frontAssets/css/main-style.min.css') }}">
 <!-- Toastr CSS -->
-<link rel="preload" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 <!-- Leaflet CSS -->
+
+<link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/tekmino-icon.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/nice-select.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/swiper.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/venobox.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/leaflet.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/meanmenu.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}" />

ac37c4736a575af3e9ee6e6d844c0f40ccc61b05
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 15:22:54 +0500

    Update css.blade.php

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -1,7 +1,3 @@
-<!-- Toastr CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-<!-- Leaflet CSS -->
-
 <link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('frontAssets/css/tekmino-icon.css') }}" />
 <link rel="stylesheet" href="{{ asset('frontAssets/css/nice-select.css') }}" />
@@ -10,3 +6,6 @@
 <link rel="preload" href="{{ asset('frontAssets/css/leaflet.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
 <link rel="preload" href="{{ asset('frontAssets/css/meanmenu.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
 <link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}" />
+
+<!-- Toastr CDN -->
+<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">

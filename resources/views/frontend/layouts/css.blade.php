1f5c3773c538eccce0ff5927ca6cb93225c70000
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 14:32:19 +0500

    css chnages

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -1,15 +1,12 @@
-<!-- Critical CSS -->
-<link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}">
+<!-- Toastr CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<!-- Leaflet CSS -->
 
-<!-- Preload Non Critical -->
-<link rel="preload" href="{{ asset('frontAssets/css/tekmino-icon.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/swiper.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/venobox.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/leaflet.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/nice-select.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/meanmenu.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-
-<!-- Toastr CDN -->
-<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-
+<link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/tekmino-icon.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/nice-select.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/swiper.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/venobox.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/leaflet.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/meanmenu.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('frontAssets/css/main.min.css') }}" />

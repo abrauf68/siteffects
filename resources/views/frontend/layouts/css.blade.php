348688962c000cc3bacb2751970fd0fae0b905a3
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 13:49:31 +0500

    Update css.blade.php

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -1,14 +1,24 @@
+<!-- Preconnect Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<!-- Google Font -->
+<link rel="preload"
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+      as="style"
+      onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+</noscript>
+
 <!-- Critical CSS -->
 <link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}">
 
-<!-- Preload Non Critical -->
-<link rel="preload" href="{{ asset('frontAssets/css/swiper.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/venobox.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/leaflet.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/nice-select.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('frontAssets/css/meanmenu.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-
-<!-- Toastr CDN -->
-<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-
+<!-- Non Critical CSS -->
+<link rel="stylesheet" href="{{ asset('frontAssets/css/swiper.min.css') }}" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="{{ asset('frontAssets/css/venobox.min.css') }}" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="{{ asset('frontAssets/css/leaflet.css') }}" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="{{ asset('frontAssets/css/nice-select.css') }}" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="{{ asset('frontAssets/css/meanmenu.css') }}" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" media="print" onload="this.media='all'">

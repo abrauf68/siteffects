f89f02a5c946e1df0dc0a67174262136e6f65478
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 14:05:08 +0500

    Update css.blade.php

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}">
 
 <!-- Preload Non Critical -->
+<link rel="preload" href="{{ asset('frontAssets/css/tekmino-icon.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{ asset('frontAssets/css/swiper.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{ asset('frontAssets/css/venobox.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{ asset('frontAssets/css/leaflet.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">

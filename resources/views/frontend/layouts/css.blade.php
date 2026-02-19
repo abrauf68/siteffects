30f513b7f8ecfde140b4279b2dbf0de62e66b1dc
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 15:10:55 +0500

    one

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="{{ asset('frontAssets/css/tekmino-icon.css') }}" />
 <link rel="stylesheet" href="{{ asset('frontAssets/css/nice-select.css') }}" />
 <link rel="stylesheet" href="{{ asset('frontAssets/css/swiper.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/venobox.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/leaflet.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/meanmenu.css') }}" />
+<link rel="preload" href="{{ asset('frontAssets/css/venobox.min.css') }}" />
+<link rel="preload" href="{{ asset('frontAssets/css/leaflet.css') }}" />
+<link rel="preload" href="{{ asset('frontAssets/css/meanmenu.css') }}" />
 <link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}" />

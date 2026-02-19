1320657a45a381945746214031e85310da414b02
hatim997 <laravel.supersoft@gmail.com>
2026-02-19 13:43:42 +0500

    Update css.blade.php

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -1,12 +1,14 @@
-<!-- Toastr CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-<!-- Leaflet CSS -->
+<!-- Critical CSS -->
+<link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}">
 
-<link rel="stylesheet" href="{{ asset('frontAssets/css/bootstrap.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/tekmino-icon.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/nice-select.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/swiper.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/venobox.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/leaflet.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/meanmenu.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontAssets/css/main.css') }}" />
+<!-- Preload Non Critical -->
+<link rel="preload" href="{{ asset('frontAssets/css/swiper.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{ asset('frontAssets/css/venobox.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{ asset('frontAssets/css/leaflet.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{ asset('frontAssets/css/nice-select.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{ asset('frontAssets/css/meanmenu.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+
+<!-- Toastr CDN -->
+<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+

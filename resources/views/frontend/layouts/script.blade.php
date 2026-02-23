9b0198a50b1ed7a00f969ba478721763b29c02d7
hatim997 <laravel.supersoft@gmail.com>
2026-02-23 14:56:08 +0500

    Update script.blade.php

--- resources/views/frontend/layouts/script.blade.php
+++ resources/views/frontend/layouts/script.blade.php
@@ -1,21 +1,29 @@
 <!-- Plugin JS File -->
-<script src="{{ asset('frontAssets/js/jquery.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/gsap.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/gsap-scroll-smoother.js') }}"></script>
-<script src="{{ asset('frontAssets/js/gsap-scroll-to-plugin.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/gsap-scroll-trigger.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/gsap-split-text.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/jquery.nice-select.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/swiper.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/waypoints.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/counterup.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/venobox.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/meanmenu.js') }}"></script>
-<script src="{{ asset('frontAssets/js/gsap-animation.js') }}"></script>
-<script src="{{ asset('frontAssets/js/matter.min.js') }}"></script>
-<script src="{{ asset('frontAssets/js/throwable.js') }}"></script>
-<script src="{{ asset('frontAssets/js/main.js') }}"></script>
+<!-- Core JS Files -->
+<script src="{{ asset('frontAssets/js/jquery.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/bootstrap.bundle.min.js') }}" defer></script>
+
+<!-- GSAP Plugins -->
+<script src="{{ asset('frontAssets/js/gsap.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/gsap-scroll-smoother.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/gsap-scroll-to-plugin.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/gsap-scroll-trigger.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/gsap-split-text.min.js') }}" defer></script>
+
+<!-- Other Plugins -->
+<script src="{{ asset('frontAssets/js/jquery.nice-select.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/swiper.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/waypoints.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/counterup.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/venobox.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/meanmenu.js') }}" defer></script>
+
+<!-- Custom Scripts -->
+<script src="{{ asset('frontAssets/js/gsap-animation.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/matter.min.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/throwable.js') }}" defer></script>
+<script src="{{ asset('frontAssets/js/main.js') }}" defer></script>
+
 <!-- Leaflet JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
 
